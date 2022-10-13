d3b393916c87e26e163727c737c1c7cb9036b0b7
julian <juliandecastelli@gmail.com>
2022-10-13 11:47:24 -0300

    add ImputHandler

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-class Player {
+export default class Player {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
@@ -7,12 +7,22 @@ class Player {
     this.image = document.getElementById("dog");
     this.width = 200;
     this.height = 181.83;
-    this.x = 0;
-    this.y = 0;
+    this.x = this.gameWidth * 0.5 - this.width * 0.5;
+    this.y = this.gameHeight - this.height;
     this.frameX = 0;
     this.frameY = 0;
   }
   draw(context) {
-    context.draw(this.image, this.x, this.y);
+    context.drawImage(
+      this.image,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+import Player from "./player.js";
+import ImputHandler from "./input.js";
+import { drawStatusText } from "./utils.js";
+
 window.addEventListener("load", () => {
   const loading = document.getElementById("loading");
   loading.style.display = "none";
-  const canvas = document.getElementById("cnvas1");
+  const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
+  const input = new ImputHandler();
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawStatusText(ctx, input);
+    requestAnimationFrame(animate);
+  }
+  animate(0);
 });
